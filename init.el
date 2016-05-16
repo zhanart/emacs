6279dd16450c25bb6c70a3fb7264e3608f48d907
arthur <jiehua.zhang@hotmail.com>
2016-05-17 07:22:24 +0800

    some initial config

---
+++ init.el
@@ -0,0 +1,12 @@
+(setq inhibit-startup-message t)
+(setq indent-tabs-mode t)
+
+(load-theme 'misterioso)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(ido-mode)
+(column-number-mode)
+(linum-mode)
